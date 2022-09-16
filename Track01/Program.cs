a9863785d1f314afa5beeed30f7cd1e5eab06116
Denis <denis1993@yandex.ru>
2022-09-16 12:40:05 +0300

    add new file track01

---
+++ Track01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+Console.WriteLine("Введите первое число : a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число : b");
+int b = Convert.ToInt32(Console.ReadLine());
+int number = b * b;
+
+if(number == a){
+    Console.WriteLine("число a является кавдратом числа b");
+}
+else{
+    Console.WriteLine("число a не является квадратом числа b");
+}
+
