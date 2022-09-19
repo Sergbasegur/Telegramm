d2a5d3b8b758ce702d9e25e80a06fc2ebfea3a49
Denis <denis1993@yandex.ru>
2022-09-19 13:38:11 +0300

    add file

---
+++ Track05/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection) {
+    int lendth = collection.Length;
+    int index = 0;
+
+    while(index < lendth) {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }   
+}
+void PrintArray(int[] col) {
+    int count = col.Length;
+    int position = 0;
+    while(position < count) {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array2 = new int[10];
+
+FillArray(array2);
+PrintArray(array2);
+
+
+
+
+
+
