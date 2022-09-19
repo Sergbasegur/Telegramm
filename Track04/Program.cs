1b01521f479c5bde56ffddaf28f26d5feb54ea45
Denis <denis1993@yandex.ru>
2022-09-19 13:15:26 +0300

    add new function

---
+++ Track04/Program.cs
@@ -0,0 +1,34 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+
+}
+
+int[] array = {11,22,33,41,53,67,77,88,9};
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
+
+int[] array1 = {1,2,3,4,5,21, 16, 21,56}; 
+int n = array1.Length;
+int find = 21;
+int index = 0;
+
+while(index < n) {
+    if(array1[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+
+    index ++;
+}
+
+
+
+
