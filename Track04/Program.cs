246ed61eaf5bc2b1d6c2528b22cd07c0d906a3b4
Denis <denis1993@yandex.ru>
2022-09-19 13:34:18 +0300

    add new file

--- Track04/Program.cs
+++ Track04/Program.cs
@@ -29,6 +29,29 @@ while(index < n) {
     index ++;
 }
 
+void FillArray(int[] collection) {
+    int lendth = collection.Length;
+    int index = 0;
+
+    while(index < lendth) {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }   
+}
+void PrintArray(int[] col) {
+    int count = col.Length;
+    int position = 0;
+    while(position < count) {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array2 = new int[10];
+
+FillArray(array2);
+PrintArray(array2);
+
+
 
 
 
