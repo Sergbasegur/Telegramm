5d9aa65050e9f7bcd78dc73a4b0db38e60dccf8a
Denis <denis1993@yandex.ru>
2022-09-19 12:49:42 +0300

    add function

--- Track03/Program.cs
+++ Track03/Program.cs
@@ -11,7 +11,7 @@ int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
-int b2 = 23;
+int b2 = 230;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
@@ -23,6 +23,11 @@ int max3 = Max(a3, b3, c3);
 
 int max = Max(max1, max2, max3);
 
+//int max = Max(
+ //   Max(a1, b1, c1),
+ //   Max(a2, b2, c2),
+ //   Max(a3, b3, c3)
+//);
 
 
 Console.WriteLine(max);
