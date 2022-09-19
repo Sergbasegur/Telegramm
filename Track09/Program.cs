b35861f0e5b0a6b2d6ef64c30a49d22e4964f4ff
Denis <denis1993@yandex.ru>
2022-09-19 16:35:32 +0300

    Добавили задачу № 8

---
+++ Track09/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i <= N; i++){
+    if(i % 2 == 0){
+        Console.WriteLine("Это целые числа :" + "{0}", i);
+    }
+}
+
