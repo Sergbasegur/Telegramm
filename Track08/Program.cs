14cf5025eaad77d4c97b56fdad41ec0d801b342d
Denis <denis1993@yandex.ru>
2022-09-19 15:18:32 +0300

    добавили задачу № 6

---
+++ Track08/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Домашняя задача № 6");
+
+Console.WriteLine("Введите  число : ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+if (x % 2 == 0 ) {
+
+    Console.WriteLine("Вы ввели четное число");
+}
+else {
+    Console.WriteLine("Вы ввели нечетное число");
+}
+
+
+
